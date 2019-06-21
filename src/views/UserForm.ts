fca203adf3d40b138bb69756c858c2415826ed50
RashikAnsar <sk.ansar46@gmail.com>
2019-06-22 00:12:32 +0530

    extending view with generics

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,7 +1,7 @@
-import { User } from '../models/User';
+import { User, UserInfo } from '../models/User';
 import { View } from './View';
 
-export class UserForm extends View {
+export class UserForm extends View<User, UserInfo> {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,

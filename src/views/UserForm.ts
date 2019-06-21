99bbdf4cc8063a9e0c54f7ec1f5bf8f1c79cddfa
RashikAnsar <sk.ansar46@gmail.com>
2019-06-21 23:12:51 +0530

    adding method to model

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,10 +1,11 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover
+      'click:.set-age': this.onSetAgeClick
     };
   }
 
@@ -18,20 +19,19 @@ export class UserForm {
     }
   }
 
-  onButtonClick(): void {
-    console.log('Button Clicked');
-  }
-
-  onHeaderHover(): void {
-    console.log('hover on h1');
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
   template(): string {
     return `
     <div>
     <h1>User Form</h1>
+    <div>User name: ${this.model.get('name')}</div>
+    <div>User name: ${this.model.get('age')}</div>
     <input />
     <button>Click me</button>
+    <button class="set-age">Set Random Age</button>
     </div>
     `;
   }

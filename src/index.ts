b7bb0f46ea19c7fbf8bfd8803017accba7b7c6d8
RashikAnsar <sk.ansar46@gmail.com>
2019-06-21 09:48:11 +0530

    Model user settern

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'John', age: 20 });
+const user = new User({});
+
+user.set({ name: 'Jane', age: 24 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -8,13 +8,15 @@
  * UserInfo type consists of name and age
  */
 interface UserInfo {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
 /**
  * User class
  * @param {Object} data is object with name and age
+ * @example new User({})
+ * @example new User({name: 'John'})
  * @example new User({name: 'John', age: 20})
  */
 export class User {
@@ -27,5 +29,16 @@ export class User {
    */
   get(propName: string): number | string {
     return this.data[propName];
+  }
+
+  /**
+   *
+   * @param {Object} update is object with user and age keys
+   * @example user.set({name: 'Jane', age: 24})
+   * @example user.set({name: 'Jane'})
+   * @example user.set({age: 20})
+   */
+  set(update: UserInfo): void {
+    Object.assign(this.data, update);
   }
 }

72b68d1d88295e822d61e9c7a8d97075e693e663
RashikAnsar <sk.ansar46@gmail.com>
2019-06-21 23:01:19 +0530

    binding events to renderer

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -3,12 +3,27 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover
     };
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   onButtonClick(): void {
     console.log('Button Clicked');
+  }
+
+  onHeaderHover(): void {
+    console.log('hover on h1');
   }
 
   template(): string {
@@ -25,6 +40,7 @@ export class UserForm {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
   }
 }
